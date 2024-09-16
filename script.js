9e1d9a8e2dcbf2fbe67da14043a4867f3950bcb0
ermerm1888 <ferreteriapunta2024@gmail.com>
2024-09-16 15:30:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,4 +5,3 @@ document.getElementById('open-card').addEventListener('click', function() {
   document.getElementById('close-card').addEventListener('click', function() {
     document.querySelector('.card').classList.remove('open');
   });
-  
